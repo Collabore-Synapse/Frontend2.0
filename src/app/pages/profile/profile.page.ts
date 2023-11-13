b62fd9c127a895fb460cc512243b81dcdc38f979
Daniel <danieldodesign2005@gmail.com>
2023-11-13 08:43:32 -0300

    feat: creation location_screen and others changes

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -12,4 +12,46 @@ export class ProfilePage implements OnInit {
   ngOnInit() {
   }
 
+  profile: String = '';
+  userName: String = 'UserAPI';
+
+  imagePaths: string[] = [
+    '../../../assets/coreto.png',
+    '../../../assets/paisagem1.jpg',
+    '../../../assets/paisagem2.jpg',
+    '../../../assets/paisagem3.jpg',
+    '../../../assets/paisagem1.jpg',
+    '../../../assets/paisagem3.jpg',
+    '../../../assets/paisagem1.jpg',
+    '../../../assets/paisagem2.jpg',
+    '../../../assets/paisagem3.jpg',
+    '../../../assets/paisagem4.jpeg',
+    '../../../assets/coreto.png',
+    '../../../assets/paisagem1.jpg',
+    '../../../assets/paisagem2.jpg',
+    '../../../assets/paisagem3.jpg',
+    '../../../assets/paisagem1.jpg',
+    '../../../assets/paisagem3.jpg',
+    '../../../assets/paisagem1.jpg',
+    '../../../assets/paisagem2.jpg',
+    '../../../assets/paisagem3.jpg',
+    '../../../assets/paisagem4.jpeg',
+    '../../../assets/coreto.png',
+    '../../../assets/paisagem1.jpg',
+    '../../../assets/paisagem2.jpg',
+    '../../../assets/paisagem3.jpg',
+    '../../../assets/paisagem1.jpg',
+    '../../../assets/paisagem3.jpg',
+    '../../../assets/paisagem1.jpg',
+    '../../../assets/paisagem2.jpg',
+    '../../../assets/paisagem3.jpg',
+    '../../../assets/paisagem4.jpeg',
+    // para chmar a API
+  ];
+
+  openModal(imagePath: string) {
+    // Implemente a lógica para abrir um modal com a imagem em imagePath
+    // Você pode usar bibliotecas de modais como o ng-bootstrap ou criar um modal personalizado.
+  }
+
 }
