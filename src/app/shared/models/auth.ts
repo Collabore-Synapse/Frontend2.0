f5a0171b13b1ba39906c3270c1d54f733e44eae6
matheusgmm <matheus.gmuccio@gmail.com>
2023-11-13 08:51:54 -0300

    Feat: Criando interfaces

---
+++ src/app/shared/models/auth.ts
@@ -0,0 +1,18 @@
+export interface Login {
+    name: string;
+    password: string;
+}
+
+
+export interface GoogleInfos {
+    name: string;
+    email: string;
+    google_id: string;
+    photo_url: string;
+}
+
+export interface CreateAccount {
+    name: string;
+    email: string;
+    password: string;
+}
