83f44c4a697ebfbe7364c7ce0f829a5d0ea4cca5
matheusgmm <matheus.gmuccio@gmail.com>
2023-11-28 15:54:52 -0300

    fix: storage methods

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -5,8 +5,9 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, switchMap } from 'rxjs';
 import { AuthService } from './shared/services/auth.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -14,16 +15,19 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private authService: AuthService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-
-    const token = this.authService.returnToken()
-    if(token == ``){
-      return next.handle(request);
+    if (request.url.includes(environment.API)) {
+      return this.authService.getToken().pipe(
+        switchMap((token) => {
+          if (token) {
+            request = request.clone({
+              setHeaders: { Authorization: `Bearer ${token}` }
+            });
+          }
+          return next.handle(request);
+        })
+      );
     }
-    request = request.clone({
-      setHeaders: {
-         Authorization: 'Bearer ' + token 
-        } 
-     });
-     return next.handle(request);
+
+    return next.handle(request);
   }
 }
